a1ed2562a2e23d7db02c7f21e14420957584c5f3
Eymeric Dechelette <hatchchien@protonmail.com>
2022-11-03 11:56:28 +0100

    fix and change default

--- asciimathfilter.js
+++ asciimathfilter.js
@@ -6,15 +6,26 @@ var pandoc = require('pandoc-filter');
 var asciimath = require('./asciimath-based/ASCIIMathTeXImg.js');
 var Formula = pandoc.Formula; // for Math
 
-function action(type,value,format,meta) {
-    if (type === 'Math' && value.length == 2 && value[1].length >= 2 && value[1].slice(0,2) === ':a') {
-        switch(value[0]['t']) {
-            case 'DisplayMath':
-            case 'InlineMath':
-                var latexout = "" + asciimath.AMTparseAMtoTeX(value[1].slice(2));
-                return Formula(value[0], latexout);
+function action({ t: type, c: value }, format, meta) {
+    // console.log(type);
+    // console.log(value);
+    // console.log(type);
+    // if (type === 'Math') {
+    //     console.log(!(value.length == 2 && value[1].length >= 2 && value[1].slice(0, 2) === ':l'));
+    // }
+    if (type === 'Math') {
+        if ((value.length == 2 && value[1].length >= 2 && value[1].slice(0, 2) === ':l')) {
+            return Formula(value[0], value[1].slice(2));
+        } else {
+            switch (value[0]['t']) {
+                case 'DisplayMath':
+                case 'InlineMath':
+                    var latexout = "" + asciimath.AMTparseAMtoTeX(value[1]);
+                    return Formula(value[0], latexout);
+            }
         }
     }
 }
+
 
 pandoc.stdio(action);
