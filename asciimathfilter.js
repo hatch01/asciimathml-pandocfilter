d6baf738305dae6e02a80694f33c71b75b1e7f8a
Eymeric Dechelette <hatchchien@protonmail.com>
2022-11-03 20:54:26 +0100

    cleanup

--- asciimathfilter.js
+++ asciimathfilter.js
@@ -8,15 +8,12 @@ var Formula = pandoc.Formula; // for Math
 
 function action({ t: type, c: value }, format, meta) {
     if (type === 'Math') {
-        console.log("math");
         if ((value.length == 2 && value[1].length >= 2 && value[1].slice(0, 2) === ':l')) {
             return Formula(value[0], value[1].slice(2));
         } else {
-            console.log(value[0]['t']);
             switch (value[0]['t']) {
                 case 'DisplayMath':
                 case 'InlineMath':
-                    console.log(value[1]);
                     var latexout = "" + asciimath.AMTparseAMtoTeX(value[1]);
                     return Formula(value[0], latexout);
             }
