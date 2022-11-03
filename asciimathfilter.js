892024e7c93cdb2e4c6d77356f62155d99a0aeb2
Eymeric Dechelette <hatchchien@protonmail.com>
2022-11-03 19:38:32 +0100

    tmp commit

--- asciimathfilter.js
+++ asciimathfilter.js
@@ -7,19 +7,22 @@ var asciimath = require('./asciimath-based/ASCIIMathTeXImg.js');
 var Formula = pandoc.Formula; // for Math
 
 function action({ t: type, c: value }, format, meta) {
-    // console.log(type);
-    // console.log(value);
+    console.log(type);
+    console.log(value);
     // console.log(type);
     // if (type === 'Math') {
     //     console.log(!(value.length == 2 && value[1].length >= 2 && value[1].slice(0, 2) === ':l'));
     // }
     if (type === 'Math') {
+        console.log("math");
         if ((value.length == 2 && value[1].length >= 2 && value[1].slice(0, 2) === ':l')) {
             return Formula(value[0], value[1].slice(2));
         } else {
+            console.log(value[0]['t']);
             switch (value[0]['t']) {
                 case 'DisplayMath':
                 case 'InlineMath':
+                    console.log(value[1]);
                     var latexout = "" + asciimath.AMTparseAMtoTeX(value[1]);
                     return Formula(value[0], latexout);
             }
